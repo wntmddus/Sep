4081d15f4dced7adea5780c227cb887795c87745
Seung Yeon Joo <wntmddussla@hotmail.com>
2017-04-09 01:13:29 -0700

    2 stack and queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,33 @@
 class MyQueue
   attr_accessor :head
   attr_accessor :tail
-
+  attr_accessor :queue
+# can I only use self.queue when I have it declared it as attr_accessor :queue?
+#
   def initialize
-    @queue = Array.new
-    @head = @queue[0]
+    self.queue = []
+    self.head = self.queue[0]
+    self.tail = self.queue[0]
   end
 
   def enqueue(element)
+    self.queue[self.queue.length] = element
+    self.head = self.queue.last
+    self.tail = self.queue.first
+
   end
 
   def dequeue
+    self.queue.delete_at(0)
+    self.head = self.queue.last
+    self.tail = self.queue.first
   end
 
   def empty?
+    if self.queue == []
+      true
+    else
+      false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,17 +1,28 @@
 class MyStack
   attr_accessor :top
+  attr_accessor :stack
 
   def initialize
-    @stack = Array.new
+    self.stack = Array.new
     self.top = nil
   end
 
   def push(item)
+    self.stack[self.stack.length] = item
+    self.top = item
   end
 
   def pop
+    item = self.stack.delete_at(self.stack.length-1)
+    self.top = self.stack.last
+    item
   end
 
   def empty?
+    if self.stack == []
+      true
+    else
+      false
+    end
   end
 end
